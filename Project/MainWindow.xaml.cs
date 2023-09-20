b89745a3f47b25f7803903ef2bb05ed8d8fb485d
Kutu_vk87@student.itstep.org <Kutu_vk87@student.itstep.org>
2023-09-20 17:49:21 +0300

    up

--- Project/MainWindow.xaml.cs
+++ Project/MainWindow.xaml.cs
@@ -14,6 +14,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
+using System.Reflection;
+using System.Security.AccessControl;
+using System.Security.Cryptography;
 
 namespace Project
 {
@@ -28,25 +32,69 @@ namespace Project
 
         public MainWindow()
         {
-            string path = System.IO.Path.GetFullPath(this.ToString());
             InitializeComponent();
-            path.Remove(path.Length - 17);
-            string path1 = System.IO.Path.GetFileName(path + "\\Рецепт.txt");
+            Bl.Items.Add("Перша страва");
+            Bl.Items.Add("Друга страва");
+            Bl.Items.Add("Салат");
+            Bl.Items.Add("Соус");
+            Bl.Items.Add("Десерт");
+            Bl.Items.Add("Выпечка");
+            Bl.Items.Add("Закуски");
+            Bl.Items.Add("Напиток");
+            path = System.IO.Path.GetFullPath(this.ToString());
+            path= path.Remove(path.Length - 19);
+            path1 = System.IO.Path.GetFullPath(path + "\\Перша страва\\Украинская кухня\\Рецепт.txt");
+            Open();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SaveAt();
+        }
+        public class Recipe
+        {
+            public string TypeFood { get; set; }
+            public string KitchenName { get; set; }
+            public string FoodName { get; set; }
+            public string PhotoLink { get; set; }
+            public string Ingredients { get; set; }
+            public string Guide { get; set; }
+        }
+        private void SaveAt()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "(*.txt)|*.txt|All files(*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == DialogResult.HasValue)
+                return;
+            string filename = saveFileDialog.FileName;
+            System.IO.File.WriteAllText(filename, $"{name.Text}\n\n{Bl.SelectedValue}\n\n{kyxna.Text}\n\n{linc}\n\n{recipe.Text}\n{desc.Text}");
+            MessageBox.Show("Файл сохранен");
+        }
+        private void Open()
+        {
             StreamReader f = new StreamReader(path1);
-            name.Text=f.ReadLine();
+            name.Text = f.ReadLine();
             f.ReadLine();
             linc = "";
+            string ab = f.ReadLine();
+            for (int i = 0; i < Bl.Items.Count; i++)
+            {
+                if (Bl.Items[i].ToString() == ab)
+                    Bl.SelectedValue = Bl.Items[i];
+            }
+            f.ReadLine();
+            kyxna.Text = f.ReadLine();
+            f.ReadLine();
             for (int i = 0; ; i++)
             {
                 string line = f.ReadLine();
-                if (line !="")
+                if (line != "")
                 {
-                    linc +=line;
+                    linc += line;
                 }
                 else { break; }
             }
             picture.ImageSource = new BitmapImage(new Uri(linc));
-            for (int i = 0;; i++)
+            for (int i = 0; ; i++)
             {
                 string line = f.ReadLine();
                 if (line != "")
@@ -55,7 +103,7 @@ namespace Project
                 }
                 else { break; }
             }
-            for(int i = 0;; i++)
+            for (int i = 0; ; i++)
             {
                 string line = f.ReadLine();
                 if (line != "")
@@ -65,20 +113,6 @@ namespace Project
                 else { break; }
             }
             f.Close();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-
-            if (saveFileDialog.ShowDialog() == DialogResult.HasValue)
-                return;
-            // получаем выбранный файл
-            string filename = saveFileDialog.FileName;
-            // сохраняем текст в файл
-            System.IO.File.WriteAllText(filename, $"{name.Text}\n{linc}\n{recipe.Text}\n{desc.Text}");
-            MessageBox.Show("Файл сохранен");
         }
     }
 }
